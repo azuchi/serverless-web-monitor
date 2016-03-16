83d9abbd43899fb914ed0678f9d70d698249add7
azuchi <azuchi@haw.co.jp>
2016-03-16 23:27:20 +0900

    HTTP監視トリガーを追加

--- sites/lib/index.js
+++ sites/lib/index.js
@@ -10,9 +10,12 @@ module.exports.respond = function(event, cb) {
   return cb(null, response);
 };
 
-var path = require('path'),
+const path = require('path'),
+  q = require('q'),
+  request = require('request-promise'),
   fs = require('fs'),
   qs = require('qs'),
+  http = require('http'),
   ejs = require('ejs');
 
 module.exports.renderTemplate = function(template, params){
@@ -23,4 +26,13 @@ module.exports.renderTemplate = function(template, params){
 
 module.exports.formParams = function(paramStr) {
   return qs.parse(paramStr);
+};
+
+module.exports.checkSites = function(sites) {
+  var promises = [];
+  sites.forEach(function (site) {
+    console.log(site);
+    promises.push(request({url: site.url}));
+  });
+  return q.all(promises);
 };
---
+++ sites/trigger/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Serverless Module: Lambda Handler
+ * - Your lambda functions should be a thin wrapper around your own separate
+ * modules, to keep your code testable, reusable and AWS independent
+ * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
+ */
+
+// Require Serverless ENV vars
+var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
+
+// Require Logic
+var lib = require('../lib');
+var dynamo = require('../lib/dynamo');
+
+// Lambda Handler
+module.exports.handler = function(event, context) {
+  dynamo.getSites().then(function (sites) {
+    lib.checkSites(sites).then(function(supposedDataFromAllPromises){
+      console.log("->", supposedDataFromAllPromises);
+    }).catch(function(e){console.log(e)});
+    var response = {
+      message: "Site monitoring executed!"
+    };
+    return context.done(null, response);
+  });
+};
