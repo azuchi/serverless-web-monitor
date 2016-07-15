6ebdf4db3f60c1eb7dd85ee40d18f0622f7b45bf
Sho TAKAHASHI <mega2nd@club.kyutech.ac.jp>
2016-07-15 15:42:55 +0900

    SNS に publish する機能を実装

---
+++ sites/lib/sns.js
@@ -0,0 +1,28 @@
+const Promise = require('bluebird');
+const AWS = require('aws-sdk');
+
+const snsConfig = {
+  sessionToken:   process.env.AWS_SESSION_TOKEN,
+  region:         process.env.AWS_REGION,
+}
+const projectName = process.env.SERVERLESS_PROJECT;
+const stage = process.env.SERVERLESS_STAGE;
+const sns = new AWS.SNS(snsConfig);
+const snsTopicArn = process.env.SNS_TOPIC_ARN;
+
+module.exports.postSNS = function(msg) {
+  return new Promise(function(resolve, reject) {
+    console.log(msg.Subject);
+    console.log(msg.Message);
+    var params = {
+      Message: msg.Message,
+      Subject: msg.Subject,
+      TopicArn: snsTopicArn
+    };
+    sns.publish(params, function(err, data){
+      console.log(data);
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+};
--- sites/sns/handler.js
+++ sites/sns/handler.js
@@ -1,7 +1,16 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
-};
+// Require Logic
+var sns = require('../lib/sns');
+
+// Lambda Handler
+module.exports.handler = function(event, context) {
+  console.log(event.data);
+  var params = event.data;
+  var msg = {
+    "Subject": "[" + params['state'] + "] " + params['name'],
+    "Message": params['state'] + ": " + params['name'] + "\n URL: " + params['url'] + "\n Reason: " + params['reason']
+  }
+  sns.postSNS(msg);
+  return context.done;
+}
