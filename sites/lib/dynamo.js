7f6419d08c811d505ecf14bab89b38413ed1ad6a
Sho TAKAHASHI <mega2nd@club.kyutech.ac.jp>
2016-08-23 19:47:08 +0900

    statusCodeが変化したらdynamoDBを更新

--- sites/lib/dynamo.js
+++ sites/lib/dynamo.js
@@ -41,3 +41,24 @@ module.exports.getSites = function() {
     });
   });
 };
+
+module.exports.updateSiteState = function(site) {
+  return new Promise(function(resolve, reject) {
+    const params = {
+      TableName: sitesTable,
+      Key: {
+        "id": site.site.id
+      },
+      UpdateExpression: "set code = :code",
+      ExpressionAttributeValues: {
+        ":code":site.code
+      },
+      ReturnValues:"UPDATED_NEW"
+    };
+    console.log("Updating site statusCode...");
+    docClient.update(params, function(err, data) {
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+};
--- sites/probe/handler.js
+++ sites/probe/handler.js
@@ -2,32 +2,44 @@
 
 const rp = require('request-promise');
 const errors = require('request-promise/errors');
+const dynamo = require('../lib/dynamo');
 
 module.exports.handler = function(event, context, cb) {
-  rp({uri: event.url, resolveWithFullResponse: true}).then(function(response) {
-    const array = {
-      Date: response.headers.date,
-      Code: response.statusCode,
-      Message: response.statusMessage
-    };
+  return new Promise(function(resolve, reject) {
+    rp({uri: event.url, resolveWithFullResponse: true}).then(function(response) {
+      const statusCode = response.statusCode.toString(10);
+      const array = {
+        site: event,
+        date: response.headers.date,
+        code: statusCode,
+        message: response.statusMessage
+      };
+      return resolve(array);
+    }).catch(errors.StatusCodeError, function(error){
+      const statusCode = error.statusCode.toString(10);
+      const array = {
+        site: event,
+        date: error.response.headers.date,
+        code: statusCode,
+        message: error.error
+      };
+      return resolve(array);
+    }).catch(errors.RequestError, function(error){
+      const now = new Date().toUTCString();
+      const array = {
+        site: event,
+        date: now,
+        code: error.error.code,
+        message: error.message
+      };
+      return resolve(array);
+    });
+  }).then(function(array) {
     console.log(array);
-    return cb(null, array);
-  }).catch(errors.StatusCodeError, function(error){
-    const array = {
-      Date: error.response.headers.date,
-      Code: error.statusCode,
-      Msg: error.error
-    };
-    console.log(array);
-    return cb(null, array);
-  }).catch(errors.RequestError, function(error){
-    const now = new Date().toUTCString();
-    const array = {
-      Date: now,
-      Code: error.error.code,
-      Msg: error.message
-    };
-    console.log(array);
-    return cb(null, array);
+    if (event.code == undefined || event.code != array.code) {
+      console.log("Accessing DynamoDB...");
+      dynamo.updateSiteState(array);
+    }
+    return resolve(null, array);
   });
 };
