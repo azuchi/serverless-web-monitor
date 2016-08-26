e8f1bca096a1e3e5f1f2929026dd2c5589eadcb7
Sho TAKAHASHI <mega2nd@club.kyutech.ac.jp>
2016-08-26 16:29:53 +0900

    publish する message の構成を変更

--- sites/sns/handler.js
+++ sites/sns/handler.js
@@ -5,11 +5,13 @@ const sns = require('../lib/sns');
 
 // Lambda Handler
 module.exports.handler = function(event, context, cb) {
-  console.log(event.data);
-  const params = event.data;
+  console.log(event);
+  const params = event;
+  const site = event.site;
+
   const msg = {
-    "Subject": "[" + params['state'] + "] " + params['name'],
-    "Message": params['state'] + ": " + params['name'] + "\n URL: " + params['url'] + "\n Reason: " + params['reason']
+    "Subject": "Status changed to " + params['state'] + " - " + site['name'],
+    "Message": "Date: " + params['date'] + "\n" + "Current Status: " + params['state'] + "\n Name: " + site['name'] + "\n URL: " + site['url'] + "\n Response: " + params['code'] + " " + params['message']
   }
   sns.postSNS(msg);
 
